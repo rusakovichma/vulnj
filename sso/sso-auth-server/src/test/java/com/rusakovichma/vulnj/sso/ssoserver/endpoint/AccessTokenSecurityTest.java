9fd327d8e691dd3974986819d1174897a4074283
Mikhail_Rusakovich <Mikhail_Rusakovich@epam.com>
2020-02-24 11:29:21 +0300

    get access token security test auth add

--- sso/sso-auth-server/src/test/java/com/rusakovichma/vulnj/sso/ssoserver/endpoint/AccessTokenSecurityTest.java
+++ sso/sso-auth-server/src/test/java/com/rusakovichma/vulnj/sso/ssoserver/endpoint/AccessTokenSecurityTest.java
@@ -1,17 +1,20 @@
 package com.rusakovichma.vulnj.sso.ssoserver.endpoint;
 
+import org.junit.After;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.AdditionalMatchers.not;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
+import static org.assertj.core.api.Assertions.*;
 
 import java.security.Principal;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,7 +23,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.util.OAuth2Utils;
@@ -30,10 +40,19 @@ import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.TokenRequest;
 import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AccessTokenSecurityTest {
+
+    private final static String CLIENT_APP_ID = "clientAppId";
+    private final static String CLIENT_APP_SECRET = "clientAppSecret";
 
     @Mock
     private TokenGranter tokenGranter;
@@ -44,12 +63,14 @@ public class AccessTokenSecurityTest {
     @Mock
     private ClientDetailsService clientDetailsService;
 
-    private String clientId = "clientAppId";
     private BaseClientDetails clientDetails = new BaseClientDetails();
 
     private TokenEndpoint endpoint;
 
-    private Principal clientAuthentication = new UsernamePasswordAuthenticationToken("clientAppId", null, Collections.EMPTY_LIST);
+    private Principal clientAuthentication = new UsernamePasswordAuthenticationToken(CLIENT_APP_ID, null, Collections.EMPTY_LIST);
+
+    private BasicAuthenticationFilter filter;
+    private AuthenticationManager authenticationManager;
 
     private TokenRequest createFromParameters(Map<String, String> parameters) {
         TokenRequest request = new TokenRequest(parameters, parameters.get(OAuth2Utils.CLIENT_ID),
@@ -58,26 +79,83 @@ public class AccessTokenSecurityTest {
         return request;
     }
 
-    @Before
-    public void init() {
+    private void initTokenEndpoint(){
         endpoint = new TokenEndpoint();
         endpoint.setTokenGranter(tokenGranter);
         endpoint.setOAuth2RequestFactory(authorizationRequestFactory);
         endpoint.setClientDetailsService(clientDetailsService);
-        clientDetails.setClientId(clientId);
+        clientDetails.setClientId(CLIENT_APP_ID);
+    }
+
+    private void initTokenEndpointAuthFilter(){
+        SecurityContextHolder.clearContext();
+
+        UsernamePasswordAuthenticationToken clienaAppRequest = new UsernamePasswordAuthenticationToken(
+                CLIENT_APP_ID, CLIENT_APP_SECRET);
+
+        clienaAppRequest.setDetails(new WebAuthenticationDetails(new MockHttpServletRequest()));
+        Authentication clientAppAuth = new UsernamePasswordAuthenticationToken(CLIENT_APP_ID, CLIENT_APP_SECRET,
+                AuthorityUtils.createAuthorityList());
+
+        authenticationManager = mock(AuthenticationManager.class);
+        when(authenticationManager.authenticate(clienaAppRequest)).thenReturn(clientAppAuth);
+        when(authenticationManager.authenticate(not(eq(clienaAppRequest)))).thenThrow(
+                new BadCredentialsException(""));
+
+        filter = new BasicAuthenticationFilter(authenticationManager,
+                new BasicAuthenticationEntryPoint());
+    }
+
+    private void destroyTokenEndpointAuthContext(){
+        SecurityContextHolder.clearContext();
+    }
+
+    @Before
+    public void init() {
+        initTokenEndpoint();
+        initTokenEndpointAuthFilter();
+    }
+
+    @After
+    public void clearContext() {
+        destroyTokenEndpointAuthContext();
+    }
+
+
+    public void testOAuthTokenEndpointClientAppAuthentication(String clientAppId, String clientSecret) throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        //Client - 'clientAppId', password - 'clientAppSecret'
+        final String authEncoded = Base64.getEncoder().encodeToString(
+                String.format("%s:%s", clientAppId, clientSecret).getBytes());
+        request.addHeader("Authorization", String.format("Basic %s", authEncoded));
+        request.setServletPath("/oauth/token");
+
+        assertThat(SecurityContextHolder.getContext().getAuthentication()).isNull();
+        FilterChain chain = mock(FilterChain.class);
+        filter.doFilter(request, new MockHttpServletResponse(), chain);
+
+        verify(chain).doFilter(any(ServletRequest.class), any(ServletResponse.class));
+
+        assertThat(SecurityContextHolder.getContext().getAuthentication()).isNotNull();
+
+        assertThat(SecurityContextHolder.getContext().getAuthentication().getName())
+                .isEqualTo("clientAppId");
     }
 
     @Test
-    public void testGetAccessTokenWithScope() throws HttpRequestMethodNotSupportedException {
-
-        when(clientDetailsService.loadClientByClientId(clientId)).thenReturn(clientDetails);
+    public void testGetAccessTokenWithScope() throws Exception {
 
         HashMap<String, String> parameters = new HashMap<>();
         parameters.put("client_id", "clientAppId");
+        parameters.put("client_secret", "clientAppSecret");
         parameters.put("redirect_uri", "http://localhost:8082/clientApp/login");
         parameters.put("grant_type", "authorization_code");
         parameters.put("code", "S3SN5s");
         parameters.put("scope", "user_info");
+
+        when(clientDetailsService.loadClientByClientId(CLIENT_APP_ID)).thenReturn(clientDetails);
+
+        testOAuthTokenEndpointClientAppAuthentication(parameters.get("client_id"), parameters.get("client_secret"));
 
         final UUID accessToken = UUID.randomUUID();
         OAuth2AccessToken expectedToken = new DefaultOAuth2AccessToken(accessToken.toString());
